b0bf289631f54e3ead2e68ff9b6687165fc683ca
RusticPrism <91286109+RusticPrism@users.noreply.github.com>
2025-12-01 20:19:05 +0100

    Initial Commit Version 1.0.0

---
+++ src/client/java/de/rusticprism/kreiscraftclientmod/client/KreiscraftclientmodClient.java
@@ -0,0 +1,121 @@
+package de.rusticprism.kreiscraftclientmod.client;
+
+import com.google.gson.*;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.ModMetadata;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.PacketByteBuf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class KreiscraftclientmodClient implements ClientModInitializer {
+
+    private static final Logger log = LogManager.getLogger(KreiscraftclientmodClient.class);
+    private boolean freecam = false;
+    private boolean freecaminstalled = false;
+    private String cheats = "false";
+    private final List<String> cheatsList = new ArrayList<>();
+
+    @Override
+    public void onInitializeClient() {
+        PayloadTypeRegistry.playC2S().register(PluginMessagePacket.ID, PluginMessagePacket.CODEC);
+        checkForFreecam();
+
+        ClientPlayConnectionEvents.JOIN.register(((handler, sender, client) -> {
+            readCheats();
+            for (ModContainer mod : FabricLoader.getInstance().getAllMods()) {
+                ModMetadata meta = mod.getMetadata();
+                for (String str : cheatsList) {
+                    if (meta.getId().equalsIgnoreCase(str)) {
+                        cheats = meta.getId();
+                    }
+                    if (meta.getId().replaceAll("-", "").equalsIgnoreCase(str)) {
+                        cheats = str;
+                    }
+                }
+                if (meta.getId().contains("freecam")) {
+                    freecaminstalled = true;
+                    File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "freecam.json5");
+                    try {
+                        freecam = JsonParser.parseReader(new FileReader(file)).getAsJsonObject().getAsJsonObject("collision").getAsJsonPrimitive("ignoreAll").getAsBoolean();
+                    } catch (FileNotFoundException e) {
+                        log.error(e.fillInStackTrace());
+                        return;
+                    }
+                }
+            }
+            PacketByteBuf buf = new PacketByteBuf(io.netty.buffer.Unpooled.buffer());
+            buf.writeString("cheats:" + cheats);
+            ClientPlayNetworking.send(new PluginMessagePacket(buf));
+            buf = new PacketByteBuf(io.netty.buffer.Unpooled.buffer());
+            buf.writeString("freecam:" + freecam);
+            ClientPlayNetworking.send(new PluginMessagePacket(buf));
+        }));
+    }
+
+    public void readCheats() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("list/cheats.txt");
+        if (inputStream == null) {
+            log.error("File not found!");
+            return;
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        reader.lines().forEach(cheatsList::add);
+    }
+
+    public void checkForFreecam() {
+        AtomicInteger i = new AtomicInteger();
+            ClientTickEvents.START_CLIENT_TICK.register(mc -> {
+                if (mc.getNetworkHandler() == null) {
+                    return;
+                }
+                log.atDebug().log(i);
+                if (i.get() >= 200) {
+                    checkForXray();
+                    if (!freecaminstalled) {
+                        return;
+                    }
+                    File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "freecam.json5");
+                    try {
+                        if (JsonParser.parseReader(new FileReader(file)).getAsJsonObject().getAsJsonObject("collision").getAsJsonPrimitive("ignoreAll").getAsBoolean()) {
+                            PacketByteBuf buf = new PacketByteBuf(io.netty.buffer.Unpooled.buffer());
+                            buf.writeString("freecam:true");
+                            ClientPlayNetworking.send(new PluginMessagePacket(buf));
+                        }
+                    } catch (FileNotFoundException e) {
+                        log.error(e.fillInStackTrace());
+                        return;
+                    }
+                    i.set(0);
+                }
+                i.getAndIncrement();
+            });
+    }
+    public void checkForXray() {
+        MinecraftClient.getInstance().getResourcePackManager().getEnabledProfiles().forEach(s -> {
+            log.atDebug().log(s);
+            if (s.getId().toLowerCase().contains("xray") || s.getId().toLowerCase().contains("x-ray")) {
+                PacketByteBuf buf = new PacketByteBuf(io.netty.buffer.Unpooled.buffer());
+                buf.writeString("xray:" + s.getId().replaceAll("file/", ""));
+                ClientPlayNetworking.send(new PluginMessagePacket(buf));
+                return;
+            }
+            if(s.getDescription().getLiteralString() != null && (s.getDescription().getLiteralString().toLowerCase().contains("xray") || s.getDescription().getLiteralString().toLowerCase().contains("x-ray")))  {
+                PacketByteBuf buf = new PacketByteBuf(io.netty.buffer.Unpooled.buffer());
+                buf.writeString("xray:" + s.getId().replaceAll("file/", ""));
+                ClientPlayNetworking.send(new PluginMessagePacket(buf));
+            }
+        });
+    }
+}
---
+++ src/client/java/de/rusticprism/kreiscraftclientmod/client/PluginMessagePacket.java
@@ -0,0 +1,57 @@
+package de.rusticprism.kreiscraftclientmod.client;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class PluginMessagePacket implements CustomPayload{
+
+    private final byte[] data;
+
+    public PluginMessagePacket(byte[] data) {
+        this.data = data;
+    }
+    public PluginMessagePacket(ByteBuf buf) {
+        this(getWrittenBytes(buf));
+    }
+    public static Id<PluginMessagePacket> ID = new Id<>(Identifier.of("kreiscraft", "mod_checker"));
+
+    public static PacketCodec<ByteBuf, PluginMessagePacket> CODEC = PacketCodec.ofStatic(
+            (buf, value) -> writeBytes(buf, value.data),
+            PluginMessagePacket::new);
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return new Id<>(Identifier.of("kreiscraft", "mod_checker"));
+    }
+
+    private static void writeBytes(ByteBuf buf, byte[] v) {
+        buf.writeBytes(v);
+    }
+    private static byte[] getWrittenBytes(ByteBuf buf) {
+        byte[] bs = new byte[buf.readableBytes()];
+        buf.readBytes(bs);
+        return bs;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PluginMessagePacket that = (PluginMessagePacket) o;
+        return Objects.deepEquals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(data);
+    }
+}
